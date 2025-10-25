9f3bc280c037fca637f5ff13e5fceaeb8320d06b
GiancarloGuarnizoBellido <128627602+GiancarloGuarnizoBellido@users.noreply.github.com>
2025-10-25 18:31:16 -0500

    Update dataset_seg.py

--- dataset_seg.py
+++ dataset_seg.py
@@ -1,247 +1,76 @@
-import lightning as L
+import os
+import numpy as np
+import pandas as pd
 import torch
-from torch import nn
-import utils
-import torchmetrics
-from models_arq.att_unet_seg import attention_unet_seg # attention_unet
-from loss import BCEDiceLoss, DiceLoss, FocalLoss, BCELogitsLoss, BCELoss, FocalLoss_weights, SigmoidFocalLoss, TopologicalPoolingLoss
-from torchmetrics.classification import BinaryJaccardIndex, BinaryPrecision, BinaryRecall, BinaryAccuracy, BinaryF1Score,BinaryStatScores, Dice
-from torchmetrics.classification import MulticlassJaccardIndex, MulticlassPrecision, MulticlassRecall, MulticlassAccuracy, MulticlassF1Score, MulticlassStatScores
-from torchmetrics.classification import Dice
-from torchmetrics.detection import IntersectionOverUnion
-from torchmetrics.segmentation import GeneralizedDiceScore, MeanIoU
+import glob
+from torch.utils.data import Dataset
+#import skvideo.io
+import cv2
 
+class WSIDataset(Dataset):
+    """ Dataset for plaques segmentation/classification """
 
-class MyModel(L.LightningModule):
-    def __init__(self, model_opts, train_par):
-        super().__init__()
-        # Define your model architecture here. For example:
-        model_def = globals()[model_opts.name]
-        #self.model = model_def(input_channels=1)  # Assuming your model's __init__ doesn't take parameters
-        self.model = model_def(input_channels=1)  # Assuming your model's __init__ doesn't take parameters
-
-        self.train_par = train_par
-        self.eval_threshold = train_par.eval_threshold
-        self.num_classes=train_par.num_classes
-        # Initialize metrics for train, dev, test
-        #self.init_metrics('train')
-        self.init_metrics('val')
-        self.init_metrics('test')
-        # Define any metrics you want to use dice, tp, fp,tn, fn,precision,recall,accuracy,f1 for train,dev,test
-
-    def init_metrics(self,stage):
+    def __init__(self, meta_data, root_dir,  cache_data=False, transform=None):
         """
-        Initializes metrics for a given stage.
-        
         Args:
-        - stage (str): The stage for which to initialize the metrics. Should be 'train', 'dev', or 'test'.
+            root_dir (string): Directory with all the WSI. Each WSI has 4 folders:
+            --root_dir
+                --WSI_name
+            transform (callable, optional): Optional transform to be applied on a sample.
         """
+        df = pd.read_csv(meta_data)
+        #self.wsi_list = df.id.to_list()
+        self.wsi_list = df.wsi.to_list()
+        
+        self.root_dir = root_dir
+        self.transform = transform
+        self.cache_data = cache_data
+        self.videos_path = np.concatenate([glob.glob(os.path.join(self.root_dir, self.wsi_list[i], 'video','*.avi')) for i in range(len(self.wsi_list))])
+        self.gt_path = np.concatenate([glob.glob(os.path.join(self.root_dir, self.wsi_list[i], 'mask', '*.avi')) for i in range(len(self.wsi_list))])
 
-        # if self.num_classes == 2:           
-        #     metrics = {
-        #         #'stat_scores': BinaryStatScores(threshold=self.eval_threshold),
-        #         'Dice': Dice(average='micro',threshold=self.eval_threshold),
-        #         'IoT': BinaryJaccardIndex(threshold=self.eval_threshold),
-        #         'precision': BinaryPrecision(threshold=self.eval_threshold),
-        #         'accuracy': BinaryAccuracy(threshold=self.eval_threshold),
-        #     }  
-        # else:
-        #     metrics = {
-        #         #'stat_scores':MulticlassStatScores(num_classes=self.num_classes),
-        #         'Dice': Dice(average='micro',num_classes=self.num_classes),
-        #         'IoT': JaccardIndex(task="multiclass", num_classes=self.num_classes),
-        #         'precision': MulticlassPrecision(num_classes=self.num_classes),
-        #         'accuracy': MulticlassAccuracy(num_classes=self.num_classes),
-        #     }  
+    def load_video(self, path):
+        cap = cv2.VideoCapture(path)
+        frames = []
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frames.append(frame)
+        cap.release()
+        return np.array(frames)
 
-        if self.num_classes == 2:           
-            metrics = {
-                #'stat_scores': BinaryStatScores(threshold=self.eval_threshold),
-                'Dice (lightning AI)': Dice(average='micro',threshold=self.eval_threshold),
-                'IoU (lightning AI)': BinaryJaccardIndex(threshold=self.eval_threshold),
-                'precision': BinaryPrecision(threshold=self.eval_threshold),
-                'accuracy': BinaryAccuracy(threshold=self.eval_threshold),
-            }  
+    def __len__(self):
+        return len(self.videos_path)
+
+    def __getitem__(self, idx):
+        # if torch.is_tensor(idx):
+        #     idx = idx.tolist()
+        
+        '''
+        Load patch and mask arrays:
+        - images come from a single WSI.
+        - mask --> 1 for plaques, 0 for background.
+        '''
+        if self.cache_data is True:
+            print("Loading from cache...")
+            image = self.dataset_imgs[idx]
         else:
-            metrics = {
-                #'stat_scores':MulticlassStatScores(num_classes=self.num_classes),
-                'Dice (lightning AI)': Dice(average='micro',threshold=self.eval_threshold),
-                'IoU (lightning AI)': MulticlassJaccardIndex(num_classes=self.num_classes),
-                'precision': MulticlassPrecision(num_classes=self.num_classes),
-                'accuracy': MulticlassAccuracy(num_classes=self.num_classes),
-            }  
+            video = self.load_video(self.videos_path[idx])
+            gt_img = self.load_video(self.gt_path[idx])
+
+        vid_rgb2gray = np.zeros((1, video.shape[0], video.shape[1], video.shape[2]), dtype=np.uint8)
+        for i in range(video.shape[0]):
+            vid_rgb2gray[0, i, :, :] = np.expand_dims(np.dot(video[i], [0.2989, 0.5870, 0.1140]), axis=0)
         
-        for metric_name, metric in metrics.items():
-            setattr(self, f"{stage}_{metric_name}", metric)
+        gt_rgb2gray = np.zeros((1, gt_img.shape[0], gt_img.shape[1], gt_img.shape[2]), dtype=np.uint8)
+        # print("Original mask",gt_img.min(), gt_img.max())
+        for i in range(gt_img.shape[0]):
+            gt_rgb2gray[0, i, :, :] = np.expand_dims(np.dot(gt_img[i], [0.2989, 0.5870, 0.1140]), axis=0)
 
-    def log_metrics(self, stage, pred, label):
-        """
-        Logs metrics for a given stage.
-        
-        Args:
-        - stage (str): Stage of the model ('train', 'dev', 'test').
-        - pred (torch.Tensor): Predictions from the model.
-        - mask (torch.Tensor): True labels.
-        """
+        sample = {'image': vid_rgb2gray, 'pred': gt_rgb2gray}
 
-        metrics = ['Dice (lightning AI)', 'IoU (lightning AI)', 'precision', 'accuracy']
-        for metric in metrics:
-            metric_fn = getattr(self, f"{stage}_{metric}")
-            metric_fn(pred, label)
-            self.log(f"{stage}_{metric}", metric_fn, on_step=False, on_epoch=True)
-        
-        dice_metric,_=utils.dice_coeff_batch(pred.float(), label.float())
-        self.log(f"{stage}_Dice (formula)", dice_metric, on_step=False, on_epoch=True)
-
-            
-    def forward(self, x):
-        # Implement the forward pass
-        return self.model(x)
-    
-    
-    def training_step(self, batch, batch_idx):
-        # Implement what happens in a training step
-        img, label = batch
-        #img, label = img.to(self.device), label.to(self.device)
-        # print("img")
-        # #print(img)
-        # print(img.shape)
-        # print(img.dtype)
-        # print("label")
-        # print(label)
-        # print(label.shape)
-        # print(label.dtype)       
-        #self.pos_weights = utils.pos_weight_batch(label)
-        pred_mask = self(img)  # This uses the forward method
-        # print("pred")
-        # print(pred)
-        # print(pred.shape)
-        # print(pred.dtype)
-
-        label = (label >= self.eval_threshold).to(torch.int64)
-
-        # print("pred new dim")
-        # print(pred)
-        # print(pred.shape)
-        # print(pred.dtype)
-        loss = self.get_loss(pred_mask.float(), label.float())  # You need to define or adjust get_loss accordingly
-        # print("loss")
-        # print(loss)
-        # print(loss.shape)
-        # print(loss.dtype)
-        self.log('train_loss', loss)
-        #self.log_metrics('train', pred, label) #Delete train metrics
-        return loss
-
-
-    def validation_step(self, batch, batch_idx):
-        # Implement the validation step
-        img, label = batch
-        #img, label = img.to(self.device), label.to(self.device)
-
-        #self.pos_weights = utils.pos_weight_batch(label)
-        pred_mask = self(img)
-
-        label = (label >= self.eval_threshold).to(torch.int64)
-
-        loss = self.get_loss(pred_mask.float(), label.float())
-        self.log('val_loss', loss)
-        #print("shape: ", pred.shape, label.shape)
-        #print("type: ",pred.dtype, label.dtype)
-        pred_prob = torch.sigmoid(pred_mask)  # Obtener probabilidad
-        pred = (pred_prob >= self.eval_threshold).to(torch.int64)
-        self.log_metrics('val', pred, label)
-
-    
-    def test_step(self, batch, batch_idx):
-        # Implement the test step
-        img, label = batch
-        #img, label = img.to(self.device), label.to(self.device)
-
-        #self.pos_weights = utils.pos_weight_batch(label)
-        pred_mask = self(img)
-
-        label = (label >= self.eval_threshold).to(torch.int64)
-
-        loss = self.get_loss(pred_mask.float(), label.float())
-        self.log('test_loss', loss)
-        #print("shape: ", pred.shape, label.shape)
-        #print("type: ",pred.dtype, label.dtype)
-        pred_prob = torch.sigmoid(pred_mask)  # Obtener probabilidad
-        pred = (pred_prob >= self.eval_threshold).to(torch.int64)
-        self.log_metrics('test', pred, label)
-
-    def predict_step(self, batch, batch_idx, dataloader_idx=None):
-        # Implement the prediction step
-        img, label = batch
-        #img, label = img.to(self.device), label.to(self.device)
-
-        #self.pos_weights = utils.pos_weight_batch(label)
-        pred_mask = self(img)
-        pred_prob = torch.sigmoid(pred_mask)  # Obtener probabilidad
-        pred = (pred_prob >= self.eval_threshold).to(torch.float)
-        label = (label >= self.eval_threshold).to(torch.float)
-
-        return pred
-
-
-    def predict_step_aux(self, batch, batch_idx, dataloader_idx=None):
-        # Implement the prediction step
-        img, label = batch
-
-        #self.pos_weights = utils.pos_weight_batch(label)
-        pred_mask = self(img)
-        pred_prob = torch.sigmoid(pred_mask)  # Obtener probabilidad
-        pred = (pred_prob >= self.eval_threshold).to(torch.float)
-        label = (label >= self.eval_threshold).to(torch.float)
-
-        return pred, pred_prob, pred_mask, label
-    
-    def configure_optimizers(self):
-        # Configure optimizers and LR schedulers
-        #optimizer = torch.optim.Adam(self.parameters(), lr=self.train_par.lr)
-        #Adamw
-        optimizer=torch.optim.AdamW(self.parameters(), lr=self.train_par.lr, weight_decay=self.train_par.weight_decay)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5)
-        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
-
-    # Implement the get_loss method here, adjust it based on your original Trainer class
-    def get_loss(self, y_hat, y):
-        if self.train_par.loss_opts.name != 'default':
-            self.loss_f = globals()[self.train_par.loss_opts.name]
-        else:
-            print(y_hat.shape, y.shape, y_hat.dtype, y.dtype)
-            self.loss_f = nn.CrossEntropyLoss(y_hat, y)
-
-        if self.train_par.loss_opts.name == 'BCELogitsLoss':
-            if self.train_par.loss_opts.args.weight == 'default':
-                return self.loss_f(y_hat, y)
-            else:
-                return self.loss_f(y_hat, y, weight = self.pos_weights)
-
-        if self.train_par.loss_opts.name == 'FocalLoss':
-            return self.loss_f(y_hat, y, alpha=1, gamma=2, logits=False, reduce=True)
-        
-        if self.train_par.loss_opts.name == 'BCEDiceLoss':
-            if self.train_par.loss_opts.args.weight == 'default':
-                return self.loss_f(y_hat, y, device=self.device)
-            else:
-                return self.loss_f(y_hat, y, weight = self.train_par.loss_opts.args.weight, device=self.device)
-        
-        if self.train_par.loss_opts.name == 'DiceLoss':
-            return self.loss_f(y_hat, y)
-        
-        if self.train_par.loss_opts.name == 'BCELoss':
-            return self.loss_f(y_hat, y)     
-        
-        if self.train_par.loss_opts.name == 'FocalLoss_weights':
-            return self.loss_f(y_hat, y)         
-
-        if self.train_par.loss_opts.name == 'SigmoidFocalLoss':
-            return self.loss_f(y_hat, y)   
-         
-        if self.train_par.loss_opts.name == 'TopologicalPoolingLoss':
-            return self.loss_f(y_hat, y)           
-             
-        if self.train_par.loss_opts.name == 'TopologicalPoolingLoss_function':
-            return self.loss_f(y_hat, y)   
+        if self.transform is not None:
+            sample = self.transform(sample)
+        # sample['image']: torch.Size([1, 1, 128, 128, 128])
+        # sample['pred']: torch.Size([1])
+        return sample['image'], sample['pred']
